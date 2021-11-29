e87e714706c49a3911b3d76f4338d574e69ddb87
miharatoki <kou566533@gmail.com>
2021-11-29 15:06:41 +0900

    first commit

---
+++ Decimal_type.java
@@ -0,0 +1,13 @@
+class Decimal_type {
+  public static void main(String[] args) {
+    // 小数
+    // floatは7桁までの小数(最後にFをつける)
+    // doubleは16桁
+
+    float var01 = 1.234F;
+    double var02 = 1.23456789;
+
+    System.out.println(var01);
+    System.out.println(var02);
+  }
+}
---
+++ Integer_type.java
@@ -0,0 +1,21 @@
+class Integer_type {
+  public static void main(String[] args){
+    // 整数
+
+    // byte型は-100~100
+    // short型は-3万~3万
+    // int型は-20億~20億
+    // long型は京以上の桁(最後にLが必要)
+    byte var01 = 1;  
+    short var02 = 12345;
+    int var03 = 1234567890;
+    long var04 = 123456789000L;
+
+    System.out.println(var01);
+    System.out.println(var02);
+    System.out.println(var03);
+    System.out.println(var04);
+
+
+  }
+}
